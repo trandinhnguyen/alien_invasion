330f117cec171a0d7aa851c75d6b1e08076ed3bc
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2023-03-15 00:32:03 +0700

    init scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ from control import Controller
 from time import sleep
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 class AlienInvasion:
@@ -28,7 +29,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         # Create an instance to store game statistics
+        # and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -210,6 +213,9 @@ class AlienInvasion:
 
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the score information
+        self.sb.show_score()
 
         # Draw the play button if the game is in active
         if not self.game_active:
